823d797fc111aa35787d2c11d05c0a0652f9dd51
urgen sherpa <sherpaurgen@users.noreply.github.com>
2024-07-15 01:18:06 +0545

    add cli param for rate limit

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -26,6 +26,11 @@ type config struct {
 		maxIdleConns int
 		maxLifeTime  int
 	}
+	limiter struct {
+		rps     float64 //request per sec
+		burst   int
+		enabled bool
+	}
 }
 
 type application struct {
@@ -42,6 +47,10 @@ func main() {
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.IntVar(&cfg.db.maxLifeTime, "db-max-life-time", 10, "PostgreSQL max connection life time")
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
